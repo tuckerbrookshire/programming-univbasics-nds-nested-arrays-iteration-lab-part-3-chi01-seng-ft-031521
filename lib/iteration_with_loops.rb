7f876dd193cf25d72852616d4fe8672d09da6bd9
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 18:44:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
          if src[row_index][element_index] == src[element_index].to_s
          sentence_element = src[row_index][element_index]
-       end
+        end
       sentence << sentence_element
       element_index += 1
     end
